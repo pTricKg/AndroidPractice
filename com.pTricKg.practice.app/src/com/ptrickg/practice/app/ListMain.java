177e934410fab6459c847481e46797a25f390fb2
pTricKg <ptrickg1976@gmail.com>
2014-05-19 00:15:01 -0600

    Added second activity for sql listing.  Added menu options for going between two activities

--- com.pTricKg.practice.app/src/com/ptrickg/practice/app/ListMain.java
+++ com.pTricKg.practice.app/src/com/ptrickg/practice/app/ListMain.java
@@ -1,7 +1,10 @@
-package com.example.com.ptrickg.practice.app;
+package com.ptrickg.practice.app;
 
 import android.os.Bundle;
 import android.app.ListActivity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -39,10 +42,24 @@ public class ListMain extends ListActivity {
 				Toast.makeText(ListMain.this, String.format("%s was chosen", country), Toast.LENGTH_SHORT).show();
 				
 			}
-			
+						
 		});
 		
-		
-		
 	}
-}
+		// Creating menu option
+		@Override 
+		public boolean onCreateOptionsMenu(Menu menu) {
+			super.onCreateOptionsMenu(menu);
+			getMenuInflater().inflate(R.menu.menu, menu);
+		      return true;
+		}
+		@Override
+		public boolean onOptionsItemSelected(MenuItem item) {
+			// Set-up intent to open SQL activity
+			Intent sqlIntent = new Intent(this, ListSQL.class);
+			
+			startActivity(sqlIntent);
+			return false;
+		}
+	}
+
---
+++ com.pTricKg.practice.app/src/com/ptrickg/practice/app/ListSQL.java
@@ -0,0 +1,71 @@
+package com.ptrickg.practice.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.os.Bundle;
+import android.provider.BaseColumns;
+import android.support.v4.widget.CursorAdapter;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+public class ListSQL extends Activity{
+	
+	private CountryDB countryDatabase = null;
+	private Cursor countries = null;
+	private ListView lvCountries = null;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.listsql);
+		
+		// Database Helper
+		CursorFactory factory = null;
+		countryDatabase = new CountryDB(this, "countryDB", factory);
+		
+		// Set-up Array for columns
+		String[] columnNames = {"Name", "Pop", "Area", BaseColumns._ID};
+		// Create list view for data
+		lvCountries = (ListView) findViewById(R.id.lvCountries);
+		
+		// get integers from list view layout
+		int[] targetLayoutIDs = {R.id.textName, R.id.textPop, R.id.textArea};
+		
+		
+		// Set-up actual database and make query of all data 
+		SQLiteDatabase db = countryDatabase.getReadableDatabase();
+        countries = db.query("countries", columnNames, null, null, null, null, null);
+        
+        // Set-up cursor to display data within this activity
+        CursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.country_item,
+                countries, columnNames, targetLayoutIDs, 0);
+        lvCountries.setAdapter(adapter);
+	}
+	// Finally, must close database to prevent memory drain
+	 public void onPause() {
+	    	super.onPause();
+	    	countryDatabase.close();
+	    }
+	 
+	 // creating menu option
+	 @Override 
+		public boolean onCreateOptionsMenu(Menu menu) {
+			super.onCreateOptionsMenu(menu);
+			getMenuInflater().inflate(R.menu.menutwo, menu);
+		      return true;
+		}
+		@Override
+		public boolean onOptionsItemSelected(MenuItem item) {
+			// Set-up intent
+			Intent mainIntent = new Intent(this, ListMain.class);
+			
+			startActivity(mainIntent);
+			return false;
+		}
+
+}
