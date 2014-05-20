028f73704657b32bacb87534b2a7779d58b71480
pTricKg <ptrickg1976@gmail.com>
2014-05-20 16:14:10 -0600

    Removed unnecessary import in service activity.

--- com.pTricKg.practice.app/src/com/pTricKg/AndroidPractice/ServiceMain.java
+++ com.pTricKg.practice.app/src/com/pTricKg/AndroidPractice/ServiceMain.java
@@ -1,16 +1,11 @@
 package com.pTricKg.AndroidPractice;
 
-import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
