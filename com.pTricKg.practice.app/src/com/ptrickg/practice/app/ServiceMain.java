739aa3340a28df58f9957599f4f2a4f372a3ff38
pTricKg <ptrickg1976@gmail.com>
2014-05-19 21:48:20 -0600

    Changed coding everywhere.  Still not working.

--- com.pTricKg.practice.app/src/com/ptrickg/practice/app/ServiceMain.java
+++ com.pTricKg.practice.app/src/com/ptrickg/practice/app/ServiceMain.java
@@ -2,44 +2,90 @@ package com.ptrickg.practice.app;
 
 
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class ServiceMain extends Activity {
+	
+	private EditText numbersText;
+	private static Intent averagingIntent;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.service_main);
+		
+		// find edit text field for interaction
+		numbersText = (EditText) findViewById(R.id.editText);
 
 		
 	}
+	public void btnClicked(View view) {
+		
+		switch (view.getId()) {
+    	case R.id.btnStartService:
+    		startService(averagingIntent);
+    	case R.id.btnStopService:
+    		stopService(averagingIntent);
+    		Button startButton = (Button) findViewById(R.id.btnStartService);
+    		startButton.setEnabled(true);
+    	default:
+    		Log.e("ERROR", "A click event was generated unexpectedly");
+    	}
+		
+	}
+	// Starting service
+	// first button
+	public void startService(View v) {
+		// Set-up string array to collect input from user
+		String[] strNumbers = numbersText.getText().toString().split(" ");
+		int[] numbers = new int[strNumbers.length];
+		// setting loop to check numbers
+		// must initialize int i with 0
+		for(int i = 0; i < strNumbers.length; i++) {
+			numbers[i] = Integer.parseInt(strNumbers[i]);
+		}
+		
+		averagingIntent = new Intent(ServiceMain.this, SimpleService.class);
+		averagingIntent.putExtra("numbers", numbers);
+		
+		startService(averagingIntent);
+	}
 	
-	 public void btnClicked(View view) {
-	    	
-	    	Intent intent = new Intent(ServiceMain.this, SimpleService.class);
-	    	
-	    	switch (view.getId()) {
-	    	case R.id.btnStartService:
-	    		startService(intent);
-	    		view.setEnabled(false);
-	    		view.setAlpha(0.8f);
-	    		break;
-	    	case R.id.btnStopService:
-	    		stopService(intent);
-	    		Button startButton = (Button) findViewById(R.id.btnStartService);
-	    		startButton.setEnabled(true);
-	    		startButton.setAlpha(1.0f);
-	    		break;
-	    	default:
-	    		Log.e("ERROR", "A click event was generated unexpectedly");
-	    	}
-	    }
-	    
+	// Second button
+	public void stopService(View v) {
+		stopService(averagingIntent);
+	}
+	
+//	 @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+//	public void btnClicked(View view) {
+//	    	
+//	    	Intent intent = new Intent(ServiceMain.this, SimpleService.class);
+//	    	
+//	    	switch (view.getId()) {
+//	    	case R.id.btnStartService:
+//	    		startService(intent);
+//	    		view.setEnabled(false);
+//	    		view.setAlpha(0.8f);
+//	    		break;
+//	    	case R.id.btnStopService:
+//	    		stopService(intent);
+//	    		Button startButton = (Button) findViewById(R.id.btnStartService);
+//	    		startButton.setEnabled(true);
+//	    		startButton.setAlpha(1.0f);
+//	    		break;
+//	    	default:
+//	    		Log.e("ERROR", "A click event was generated unexpectedly");
+//	    	}
+//	    }
+//	    
 
 //	@Override
 //	public boolean onCreateOptionsMenu(Menu menu) {
@@ -51,7 +97,7 @@ public class ServiceMain extends Activity {
 //
 //	@Override
 //	public boolean onOptionsItemSelected(MenuItem item) {
-//		// Handle action bar item clicks here. The action bar will
+//	// Handle action bar item clicks here. The action bar will
 //		// automatically handle clicks on the Home/Up button, so long
 //		// as you specify a parent activity in AndroidManifest.xml.
 //		int id = item.getItemId();
@@ -60,7 +106,7 @@ public class ServiceMain extends Activity {
 //		}
 //		return super.onOptionsItemSelected(item);
 //	}
-
+//
 //	}
 
 }
--- com.pTricKg.practice.app/src/com/ptrickg/practice/app/SimpleService.java
+++ com.pTricKg.practice.app/src/com/ptrickg/practice/app/SimpleService.java
@@ -3,21 +3,24 @@ package com.ptrickg.practice.app;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
+import android.widget.Toast;
 
 public class SimpleService extends Service {
 	
 	// Set-up variables
-	private NotificationManager manager;
-	private Timer counter = new Timer();
-	private long seconds = 0L;
-	private TimerTask clockTask = null;
-	private PendingIntent notificationIntent = null;
+//	private NotificationManager manager;
+//	private Timer counter = new Timer();
+//	private long seconds = 0L;
+//	private TimerTask clockTask = null;
+//	private PendingIntent notificationIntent = null;
 	
 	// Set-up IBinder but not used yet
 	@Override
@@ -29,47 +32,67 @@ public class SimpleService extends Service {
 	// Getting service created and started
 	public void onCreate() {
 		super.onCreate();
-		manager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
-		displayNotification("Service Started");
-		clockTask = new TimerTask() {
-			public void run() {
-				seconds++;
-			}
-		};
-		startCounter();
+//		manager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+//		displayNotification("Service Started");
+//		clockTask = new TimerTask() {
+//			public void run() {
+//				seconds++;
+//			}
+//		};
+//		startCounter();
+	}
+	
+	// int created for array and starts service
+	public int onStartCommand(Intent intent, int flags, int startID) {
+		int[] numArray = intent.getExtras().getIntArray("numbers");
+		float avg = findAverage(numArray);
+		
+		Toast.makeText(getApplicationContext(), String.format("Average %f", avg), Toast.LENGTH_SHORT).show();
+		return startID;
 	}
 	
 	// Must explicitly destroy service
 	public void onDestroy() {
 		super.onDestroy();
-		stopCounter();
-		manager.cancel(R.string.notification);
-		displayNotification(String.format("Service stopped after %d seconds.", seconds));
-		seconds = 0L;
+		Toast.makeText(getApplicationContext(), "Service stopped.", Toast.LENGTH_SHORT).show();
+//		stopCounter();
+//		manager.cancel(R.string.notification);
+//		displayNotification(String.format("Service stopped after %d seconds.", seconds));
+//		seconds = 0L;
+	}
+	
+	// loops through number array to find average
+	float findAverage(int[] nums) {
+		int sum = 0;
+		for (int i = 0; i < nums.length; i++) {
+			sum += nums[i];
+		}
+		return (float)sum / nums.length;
 	}
 	
 	// Creating notification for Service state
-	private void displayNotification(CharSequence text) {
-		Notification notification = new Notification.Builder(this)
-			.setTicker(text)
-			.setContentTitle(text)
-			.setContentIntent(notificationIntent)
-			.setSmallIcon(R.drawable.ic_launcher)
-			.build();
-		
-		manager.notify(R.string.notification, notification);
-	}
-	
-	// Self explained
-	private void startCounter() {
-		//set up a timer to increment seconds once per second (1000 milliseconds):
-		counter.scheduleAtFixedRate(clockTask, 0, 1000L);
-	}
-	
-	private void stopCounter() {
-		if (counter != null) {
-			counter.cancel();
-		}
-	}
+//	@TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+//	private void displayNotification(CharSequence text) {
+//		Notification notification = new Notification.Builder(this)
+//			.setTicker(text)
+//			.setContentTitle(text)
+//			.setContentIntent(notificationIntent)
+//			.setSmallIcon(R.drawable.ic_launcher)
+//			.build();
+//		
+//		manager.notify(R.string.notification, notification);
+//	}
+//	
+//	// Self explained
+//	private void startCounter() {
+//		//set up a timer to increment seconds once per second (1000 milliseconds):
+//		counter.scheduleAtFixedRate(clockTask, 0, 1000L);
+//	}
+//	
+//	private void stopCounter() {
+//		if (counter != null) {
+//			counter.cancel();
+//		}
+//	}
 
 }
