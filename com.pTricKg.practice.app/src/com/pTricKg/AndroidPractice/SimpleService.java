57c1d312fbe3f893a46fa238523a248f7212dc0d
pTricKg <ptrickg1976@gmail.com>
2014-05-20 18:25:28 -0600

    Clean up SimpleService code and removed unused imports

--- com.pTricKg.practice.app/src/com/pTricKg/AndroidPractice/SimpleService.java
+++ com.pTricKg.practice.app/src/com/pTricKg/AndroidPractice/SimpleService.java
@@ -1,47 +1,17 @@
 package com.pTricKg.AndroidPractice;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
-import android.annotation.TargetApi;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
-import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
 
 public class SimpleService extends Service {
 
-	// Set-up variables
-	// private NotificationManager manager;
-	// private Timer counter = new Timer();
-	// private long seconds = 0L;
-	// private TimerTask clockTask = null;
-	// private PendingIntent notificationIntent = null;
-
-	// Set-up IBinder but not used yet
-	@Override
-	public IBinder onBind(Intent intent) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 	// Getting service created and started
 	public void onCreate() {
 		super.onCreate();
-		// manager =
-		// (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
-		// displayNotification("Service Started");
-		// clockTask = new TimerTask() {
-		// public void run() {
-		// seconds++;
-		// }
-		// };
-		// startCounter();
+		
 	}
 
 	// int created for array and starts service
@@ -61,11 +31,7 @@ public class SimpleService extends Service {
 		super.onDestroy();
 		Toast.makeText(getApplicationContext(), "Service stopped.",
 				Toast.LENGTH_SHORT).show();
-		// stopCounter();
-		// manager.cancel(R.string.notification);
-		// displayNotification(String.format("Service stopped after %d seconds.",
-		// seconds));
-		// seconds = 0L;
+		
 	}
 
 	// loops through number array to find average
@@ -77,30 +43,11 @@ public class SimpleService extends Service {
 		return (float) sum / nums.length;
 	}
 
-	// Creating notification for Service state
-	// @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-	// private void displayNotification(CharSequence text) {
-	// Notification notification = new Notification.Builder(this)
-	// .setTicker(text)
-	// .setContentTitle(text)
-	// .setContentIntent(notificationIntent)
-	// .setSmallIcon(R.drawable.ic_launcher)
-	// .build();
-	//
-	// manager.notify(R.string.notification, notification);
-	// }
-	//
-	// // Self explained
-	// private void startCounter() {
-	// //set up a timer to increment seconds once per second (1000
-	// milliseconds):
-	// counter.scheduleAtFixedRate(clockTask, 0, 1000L);
-	// }
-	//
-	// private void stopCounter() {
-	// if (counter != null) {
-	// counter.cancel();
-	// }
-	// }
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 
 }
