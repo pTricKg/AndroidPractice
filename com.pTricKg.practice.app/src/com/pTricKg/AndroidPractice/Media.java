c3aa293d787b585fdc44275ca723899c2f79a78a
pTricKg <ptrickg1976@gmail.com>
2014-05-22 00:39:51 -0600

    Added some mp3s to raw and coded Media more

--- com.pTricKg.practice.app/src/com/pTricKg/AndroidPractice/Media.java
+++ com.pTricKg.practice.app/src/com/pTricKg/AndroidPractice/Media.java
@@ -3,6 +3,7 @@ package com.pTricKg.AndroidPractice;
 import android.app.Activity;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
 public class Media extends Activity {
 	
@@ -15,15 +16,32 @@ public class Media extends Activity {
 	}
 	
 	public void onStop() {
+		// check if media present
 		super.onStop();
-		if (sTrekOpen == null) {
+		if (sTrekOpen != null) {
 			sTrekOpen.release();
 			sTrekOpen = null;
 		}
-		if (sTrekClose == null) {
+		if (sTrekClose != null) {
 			sTrekClose.release();
 			sTrekClose = null;
 		}
 	}
+	
+	//playing sounds
+	public void play(View v) {
+		if(sTrekOpen != null) {
+			sTrekOpen.release();
+		}
+		switch(v.getId()) {
+		
+		case R.id.btnSTOpen:
+			sTrekOpen = MediaPlayer.create(getApplicationContext(), R.raw.star_trek_open);
+			break;
+		case R.id.btnSTClose:
+			sTrekOpen = MediaPlayer.create(getApplicationContext(), R.raw.star_trek_close);
+			break;
+		}
+		}
 
 }
