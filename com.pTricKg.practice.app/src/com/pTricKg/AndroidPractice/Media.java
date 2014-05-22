5ec02eed58a57e522f62e412f7b76f49e726aaee
pTricKg <ptrickg1976@gmail.com>
2014-05-22 00:25:00 -0600

    Created some variables and some methods in Media

--- com.pTricKg.practice.app/src/com/pTricKg/AndroidPractice/Media.java
+++ com.pTricKg.practice.app/src/com/pTricKg/AndroidPractice/Media.java
@@ -1,9 +1,29 @@
 package com.pTricKg.AndroidPractice;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
+import android.os.Bundle;
 
 public class Media extends Activity {
 	
+	MediaPlayer sTrekOpen = null;
+	MediaPlayer sTrekClose = null;
 	
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.media);
+	}
+	
+	public void onStop() {
+		super.onStop();
+		if (sTrekOpen == null) {
+			sTrekOpen.release();
+			sTrekOpen = null;
+		}
+		if (sTrekClose == null) {
+			sTrekClose.release();
+			sTrekClose = null;
+		}
+	}
 
 }
