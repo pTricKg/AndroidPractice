74e9a320400f355ee4c160fa5cab0e6a52a62555
pTricKg <ptrickg1976@gmail.com>
2014-05-23 12:30:02 -0600

    Removed unused imports

--- com.pTricKg.practice.app/src/com/pTricKg/AndroidPractice/AnotherService.java
+++ com.pTricKg.practice.app/src/com/pTricKg/AndroidPractice/AnotherService.java
@@ -11,8 +11,6 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.Build;
 import android.os.IBinder;
-import android.util.Log;
-import android.widget.Toast;
 
 public class AnotherService extends Service {
 
