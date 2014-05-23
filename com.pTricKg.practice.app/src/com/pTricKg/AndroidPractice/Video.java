1c0185c6b86b4a7464efe2c620a61325f9f8056c
pTricKg <ptrickg1976@gmail.com>
2014-05-23 17:58:25 -0600

    awesome, got video to play from res/raw

--- com.pTricKg.practice.app/src/com/pTricKg/AndroidPractice/Video.java
+++ com.pTricKg.practice.app/src/com/pTricKg/AndroidPractice/Video.java
@@ -1,6 +1,7 @@
 package com.pTricKg.AndroidPractice;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.MediaController;
 import android.widget.VideoView;
@@ -24,8 +25,12 @@ public class Video extends Activity {
 		vCont = new MediaController(this);
 		//set controller to play video view
 		vView.setMediaController(vCont);
-		//set path to video
-		vView.setVideoPath("mnt/sdcard/Movies/data_asks_spock.mp4");  
+		//set path to video form res/raw
+		Uri video= Uri.parse("android.resource://com.pTricKg.AndroidPractice/" + R.raw.data_asks_spock);
+		//set path to video from sdcard
+		//vView.setVideoPath("mnt/sdcard/Movies/data_asks_spock.mp4"); 
+		// setting parsed video 
+		vView.setVideoURI(video);
 		vView.requestFocus();
 		vCont.show();
 		vView.start();
