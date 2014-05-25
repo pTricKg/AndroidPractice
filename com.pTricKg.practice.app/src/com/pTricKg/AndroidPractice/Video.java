83871829693d5c91345bcc982951d1c5d19a2955
pTricKg <ptrickg1976@gmail.com>
2014-05-24 22:21:54 -0600

    Added menu to Video

--- com.pTricKg.practice.app/src/com/pTricKg/AndroidPractice/Video.java
+++ com.pTricKg.practice.app/src/com/pTricKg/AndroidPractice/Video.java
@@ -1,8 +1,11 @@
 package com.pTricKg.AndroidPractice;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
@@ -36,6 +39,58 @@ public class Video extends Activity {
 		vView.start();
 				
 		
+	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		getMenuInflater().inflate(R.menu.menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Set-up intent
+		Intent mainIntent = new Intent(this, ListMain.class);
+		Intent sqlIntent = new Intent(this, ListSQL.class);
+		Intent serviceIntent = new Intent(this, ServiceMain.class);
+		Intent sleep = new Intent(this, IntentServiceMain.class);
+		Intent bind = new Intent(this, BoundMain.class);
+		Intent anotherService = new Intent(this, AnotherService.class);
+		Intent media = new Intent(this, Media.class);
+		Intent video = new Intent(this, Video.class);
+
+		switch (item.getItemId()) {
+		case R.id.listarray:
+			startActivity(mainIntent);
+			return true;
+		case R.id.sql:
+			startActivity(sqlIntent);
+			return true;
+		case R.id.service:
+			startActivity(serviceIntent);
+			return true;
+		case R.id.intentservice:
+			startActivity(sleep);
+			return true;
+		case R.id.binderservice:
+			startActivity(bind);
+			return true;
+		case R.id.anotherservice:
+			startActivity(anotherService);
+			return true;
+		case R.id.media:
+			startActivity(media);
+			return true;
+		case R.id.video:
+			startActivity(video);
+			return true;
+		case R.id.quit:
+			finish();
+			return true;
+
+		}
+		return false;
+
 	}
 
 }
