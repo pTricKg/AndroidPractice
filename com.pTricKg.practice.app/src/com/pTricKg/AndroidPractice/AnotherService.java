1815b4ee894c980a9cfb1f4f0dea5fd31555b6c0
pTricKg <ptrickg1976@gmail.com>
2014-05-24 22:38:00 -0600

    Small comment edit

--- com.pTricKg.practice.app/src/com/pTricKg/AndroidPractice/AnotherService.java
+++ com.pTricKg.practice.app/src/com/pTricKg/AndroidPractice/AnotherService.java
@@ -14,6 +14,7 @@ import android.os.IBinder;
 
 /*
  * Simply gives a notification in status bar for service started and service stopped.
+ * Service provider is AnotherService.java.
  */
 
 public class AnotherService extends Service {
