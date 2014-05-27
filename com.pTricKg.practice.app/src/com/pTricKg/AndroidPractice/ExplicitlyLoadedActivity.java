30b20002325e7b0898e692d3de0677e80784d933
pTricKg <ptrickg1976@gmail.com>
2014-05-26 18:27:19 -0600

    Small code edit

--- com.pTricKg.practice.app/src/com/pTricKg/AndroidPractice/ExplicitlyLoadedActivity.java
+++ com.pTricKg.practice.app/src/com/pTricKg/AndroidPractice/ExplicitlyLoadedActivity.java
@@ -47,20 +47,17 @@ public class ExplicitlyLoadedActivity extends Activity {
 
 		Log.i(TAG, "Entered enterClicked()");
 
-		// TODO - Save user provided input from the EditText field
-		mEditText.getText().toString();
-
 		// TODO - Create a new intent and save the input from the EditText field
 		// as an extra
 		Intent editIntent = new Intent(ExplicitlyLoadedActivity.this,
 				ActivityLoaderActivity.class);
-		
 
+		// TODO - Save user provided input from the EditText field
 		editIntent.putExtra("key", mEditText.getText().toString());
-		
+
 		// TODO - Set Activity's result with result code RESULT_OK
 		setResult(RESULT_OK);
-		
+
 		// TODO - Finish the Activity
 		finish();
 
